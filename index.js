dbc894c6ccf85692f21b3c4505a73b27fd75aade
Danica Valdez <danicaevaldez@gmail.com>
2020-01-23 19:20:15 -0900

    test without mail key

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 require('dotenv').config();
 const server = require('./server')
 
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`==== Server running on http://localhost:${port} ====`))
