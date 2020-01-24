42a2d4db41b76c026f91e173a714d457865c24a7
Danica Valdez <danicaevaldez@gmail.com>
2020-01-23 18:27:55 -0900

    Deploying project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // code away!
+require('dotenv').config();
 const server = require('./server')
 
-server.listen(4000, () => console.log('==== Server running on http://localhost:4000 ===='))
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`==== Server running on http://localhost:${port} ====`))
